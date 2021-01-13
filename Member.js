85f41fa846c29805407f14f0a7fd828bd0e63599
Amanda-G10 <reahana233@gmail.com>
2021-01-13 09:59:53 -1200

    Member

--- Member.js
+++ Member.js
@@ -4,7 +4,6 @@ const memberSchema = new Schema({
     name: {
         type: String,
         required: true,
-        trim: true,
         minlength:2
     },
     position: {
@@ -14,17 +13,9 @@ const memberSchema = new Schema({
     },
     birthday:{
         type: String
-    },
-    email:{
-        type: String,
-        trim: true
-    },
-    phone:{
-        type: String,
-        minlength:9,
-        maxlength:16
+        
     }
 })
 
-const Member =model('Member',memberSchema)
+const Member =model('Member', memberSchema)
 module.exports= Member
