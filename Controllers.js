f68ca568640a9a7e2455142dabf7d53f3668daeb
Amanda-G10 <reahana233@gmail.com>
2021-02-26 20:31:38 +0600

    update control

--- Controllers.js
+++ Controllers.js
@@ -26,7 +26,7 @@ exports.getOneMember =(req,res)=>{
         })
 }
 exports.addMember =(req,res)=>{
-    let { name,position, birthday, email,phone,id} = req.body
+    let { name,position, birthday,id} = req.body
 
     let error = {}
     if (!name) {
@@ -38,12 +38,7 @@ exports.addMember =(req,res)=>{
     if (!birthday) {
         error.birthday = 'Please Provide A Birthday'
     }
-    if (!email) {
-        error.email = 'Please Provide An Email'
-   }
-     if (!phone) {
-         error.phone = 'Please Provide A Phone Number'
-     }
+  
      
 
     let isError = Object.keys(error).length > 0
@@ -68,9 +63,8 @@ exports.addMember =(req,res)=>{
                 $set: {
                     name,
                     position,
-                    birthday,
-                    email,
-                    phone
+                    birthday
+                   
                     
                 }
             }
@@ -92,9 +86,8 @@ exports.addMember =(req,res)=>{
         let member = new Member({
             name,
             position,
-            birthday,
-            email,
-            phone
+            birthday
+            
             
         })
      
@@ -118,7 +111,7 @@ exports.addMember =(req,res)=>{
 
 }
 exports.editMember =(req,res)=>{
-    let { name,position, birthday,email,phone } = req.body
+    let { name,position, birthday } = req.body
     let { id } = req.params
     Contact.findOneAndUpdate(
         { _id: id },
@@ -126,9 +119,8 @@ exports.editMember =(req,res)=>{
             $set: {
             name,
             position,
-            birthday,
-            email,
-             phone
+            birthday
+            
            
             }
         },
